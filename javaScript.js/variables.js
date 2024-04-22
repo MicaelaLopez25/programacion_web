b7539b68b5fa6c91566c49a7e6bf84f743316252
micaela <mica28368@gmail.com>
2024-04-22 16:58:32 -0300

    aun me faltan 6 pokemons

---
+++ javaScript.js/variables.js
@@ -0,0 +1,20 @@
+var numero = 1; //le asignamos valor a una variable de tipo number
+var palabra = 'palabra' ;//le asginamos valor a una variable tipo string
+
+//definimos variables con la palabra let reservada let para evitar el llamado de las misma desde cualquier parte del codigo
+let numero = 2;
+let palabra2 = 'palabra 2';
+
+//definimos constantes con la palabra reservada const
+const numConstante = 4; //siempre que sepan que no van a mutar
+
+//ejemplo
+
+if (numero < 0){
+    let numero3 = 3;
+}
+
+console.log("numero", numero2); //mostrar un valor por pantalla
+
+//////////////////////////////////////////////
+unreal//para crear juego mas pro 
