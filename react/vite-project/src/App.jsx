9d00f583de3bd79c2e3b3abee99192b5f76e63c7
benjamin martinez <benjamartinez454@gmail.com>
2024-06-03 16:57:50 -0300

    primer proyecto react

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { Profile } from "./components/mi_primer_componente/MiPrimerComponente";
+
+function App() {
+  return (
+    <>
+      <Profile name={"Micaela"} lastName={"Lopez"} />
+    </>
+  );
+}
+
+export default App;
---
+++ react/vite-project/src/components/mi_primer_componente/MiPrimerComponente.jsx
@@ -0,0 +1,8 @@
+export function Profile({ name, lastName }) {
+  return (
+    <>
+      soy {name} {lastName}
+      <img src="./img/flores.jpg" alt="" />
+    </>
+  );
+}
